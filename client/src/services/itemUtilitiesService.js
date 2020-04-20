c0dffd692ca8f31e57bb4931a9b397790ad231a6
eliorav <elior.av@gmail.com>
2020-04-20 19:07:28 +0300

    sort by status

--- client/src/services/itemUtilitiesService.js
+++ client/src/services/itemUtilitiesService.js
@@ -1,3 +1,12 @@
+import {SORT_ORDER_BY_STATUS} from "../constants";
+
 export function filterTodoItemById(todoList, todoId) {
     return todoList.filter(todo => todo.id !== todoId)
+}
+
+export function sortTodoItems(todoItems) {
+    todoItems.sort((todoItem1, todoItem2) => {
+        return SORT_ORDER_BY_STATUS[todoItem1.status] - SORT_ORDER_BY_STATUS[todoItem2.status];
+    });
+    return todoItems;
 }
