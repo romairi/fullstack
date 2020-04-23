220a4d582b772a8f814389f85f1e8569cf73252d
Roman Irinarhov <pyshok1991@gmail.com>
2020-04-23 16:48:56 +0300

    add element works on port 8000

--- server/todoItem/todoItem.controller.js
+++ server/todoItem/todoItem.controller.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const TodoItemModel = require('./todoItem.model');
 const {STATUSES} = require('../constants');
 
 let todoList = _.range(5).map(idx => ({
@@ -14,17 +15,20 @@ function getItems(req, res, next) {
     res.json(todoList);
 }
 
-function create(req, res, next) {
+async function create(req, res, next) {
     const {title} = req.body;
-    todo_id++;
-    const newItem = {
-        id: `todo_id_${todo_id}`,
-        title: title,
+    const todoItem = new TodoItemModel({
+        title,
         status: STATUSES.TODO,
-        date: (new Date()),
-    };
-    todoList.push(newItem);
-    res.json(newItem);
+        date: new Date(),
+    });
+
+    try {
+        const newItem = await todoItem.save();
+        res.json(newItem);
+    } catch (e) {
+        next(e);
+    }
 }
 
 
