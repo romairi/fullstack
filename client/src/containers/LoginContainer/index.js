e335da5897b1a9a90bf0ed25b5a3e7b18a82bf44
Roman Irinarhov <pyshok1991@gmail.com>
2020-04-29 21:20:40 +0300

    routing between login and sign pages using Link

--- client/src/containers/LoginContainer/index.js
+++ client/src/containers/LoginContainer/index.js
@@ -3,22 +3,13 @@ import './index.scss';
 import {connect} from "react-redux";
 import { Link } from 'react-router-dom';
 
-
-class SignUpContainer extends React.PureComponent {
+class LoginContainer extends React.PureComponent {
     render() {
         return (
             <div className="sign-up-container">
-                <div className="title_card"><h2>Sign Up</h2></div>
+                <div className="title_card"><h2>Login</h2></div>
                 <div className="sign-up-form">
                     <form>
-                        <div className="form-group">
-                            <input
-                                type="text"
-                                className="form-control"
-                                name="username"
-                                placeholder="UserName"
-                                required="required"/>
-                        </div>
                         <div className="form-group">
                             <input
                                 type="email"
@@ -36,22 +27,14 @@ class SignUpContainer extends React.PureComponent {
                                 required="required"/>
                         </div>
                         <div className="form-group">
-                            <input
-                                type="password"
-                                className="form-control"
-                                name="confirm_password"
-                                placeholder="Confirm Password"
-                                required="required"/>
-                        </div>
-                        <div className="form-group">
                             <button type="submit" className="btn btn-primary btn-lg">
-                                Sign Up
+                                Sign In
                             </button>
                         </div>
                     </form>
                     <div className="text-center">
-                        Already have an account?
-                        <Link to="/"> Login here</Link>
+                        Don't have an account?
+                        <Link to="/signup"> Sign up</Link>
                     </div>
                 </div>
             </div>
@@ -63,4 +46,4 @@ const mapStateToProps = state => ({});
 
 const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(SignUpContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer);
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import {Route, Switch} from 'react-router'
 import TodoListContainer from "../containers/TodoListContainer";
 import SignUpContainer from "../containers/SignUpContainer";
+import LoginContainer from "../containers/LoginContainer";
 
 const routes = (
-
         <Switch>
-            <Route exact path="/" component={SignUpContainer}/>
+            <Route exact path="/" component={LoginContainer}/>
             <Route exact path="/todos" component={TodoListContainer}/>
             <Route exact path="/signup" component={SignUpContainer}/>
         </Switch>
