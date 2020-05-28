2b7570cd9ea85b5ca3f65b8079965c7222d9ae41
Roman Irinarhov <pyshok1991@gmail.com>
2020-05-28 17:12:43 +0300

    add navigate item to header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './index.scss'
+import {LOGIN_ROUTE} from '../../routes/constants';
+
 
 export default function Header() {
     return (
-        <header className="header"/>
+        <header className="header">
+            <nav className="main-nav">
+                <ul className="main-nav-items">
+                    <li className="main-nav-item">
+                        <a href='/'>Logout</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
