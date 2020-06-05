f8d0817d82feb6f8409339351e69b0a5e288be0f
Roman Irinarhov <pyshok1991@gmail.com>
2020-06-05 16:00:03 +0300

    add papers route with basic container

---
+++ client/src/containers/PaperListContainer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function PaperListContainer(props) {
+
+
+    return (
+        <div className="papers_container">
+            <div>
+                PAPERS SEARCH BOX
+            </div>
+
+
+        </div>
+    )
+}
+
+export default PaperListContainer;
