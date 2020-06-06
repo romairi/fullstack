5ae0cf1ac67a96f582c166c6f8d1c92adcc5f9c0
Roman Irinarhov <pyshok1991@gmail.com>
2020-06-06 17:23:07 +0300

    add two buttons to searchBox

--- client/src/components/SearchBox/index.js
+++ client/src/components/SearchBox/index.js
@@ -7,25 +7,41 @@ import './index.scss';
 
 function SearchBox(props) {
     const [searchParam, setSearchParam] = React.useState('');
-    const [searchPapers, setSearchPapers] = React.useState([]);
+    const [searchIncTags, setSearchIncTags] = React.useState([]);
+    const [searchExcTags, setSearchExcTags] = React.useState([]);
 
     const onSearchFieldChange = event => {
         setSearchParam(event.target.value);
     };
 
     const onIncludeButtonClicked = () => {
-        setSearchPapers([...searchPapers, searchParam]);
+        setSearchIncTags([...searchIncTags, searchParam]);
         setSearchParam('');
 
     };
 
-    const onDeleteTag = (paperId) => {
-        setSearchPapers(searchPapers.filter((paper, idx) => idx !== paperId));
+    const onExcludeButtonClicked = () => {
+        setSearchExcTags([...searchExcTags, searchParam]);
+        setSearchParam('');
     };
 
-    const includePapersElements =
-        searchPapers.map((paper, idx) =>
-            <Tag key={idx} id={idx} onDeleteClicked={onDeleteTag}>{paper}</Tag>);
+    const onDeleteTag = (tagId) => {
+        setSearchIncTags(searchIncTags.filter((tag, idx) => idx !== tagId));
+    };
+
+    const onDeleteTagExc = (tagId) => {
+        setSearchExcTags(searchExcTags.filter((tag, idx) => idx !== tagId));
+    };
+
+
+    const includeTagsElements =
+        searchIncTags.map((tag, idx) =>
+            <Tag key={idx} id={idx} onDeleteClicked={onDeleteTag}>{tag}</Tag>);
+
+    const excludeTagsElements =
+        searchExcTags.map((tag, idx) =>
+            <Tag key={idx} id={idx} onDeleteClicked={onDeleteTagExc}>{tag}</Tag>);
+
 
     return (
         <div className="search_box_container">
@@ -50,13 +66,37 @@ function SearchBox(props) {
                         variant="contained"
                         size="medium"
                         color="primary"
-                        type="submit">
+                        type="submit"
+                        onClick={onExcludeButtonClicked}>
                         exclude
                     </Button>
                 </div>
                 <div className="search_box_result">
-                    <h4>Include tags:</h4>
-                    {includePapersElements}
+                    <h4 className="title_tag">
+                        <strong>Include tags:</strong>
+                    </h4>
+                    {includeTagsElements}
+                    <h4 className="title_tag">
+                        <strong>Exclude tags:</strong>
+                    </h4>
+                    {excludeTagsElements}
+                </div>
+
+                <div className="search_box_submit">
+                    <Button
+                        variant="contained"
+                        size="medium"
+                        color="primary"
+                        type="submit">
+                        Search
+                    </Button>
+                    <Button
+                        variant="contained"
+                        size="medium"
+                        color="secondary"
+                        type="submit">
+                        Clear
+                    </Button>
                 </div>
             </Card>
         </div>
