462ac5877a92c09dec1da5c8afe5e62b5415286b
Roman Irinarhov <pyshok1991@gmail.com>
2020-07-06 21:52:44 +0300

    filter search fields

--- client/src/services/itemUtilitiesService.js
+++ client/src/services/itemUtilitiesService.js
@@ -1,5 +1,5 @@
 export function searchByFields(items, field) {
-    return items.filter(item => [item.title, item.summary]
+    return items.filter(item => [item.title, item.summary, ...item.authors.filter(a => Array.isArray(a) && a.length > 0).map(a => a[0])]
         .map(text => text.toLowerCase())
         .filter(text => text.includes(field.toLowerCase()))
         .length > 0
