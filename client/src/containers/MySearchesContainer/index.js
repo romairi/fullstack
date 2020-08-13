522243546b636eca241ce96e493dfe71f4d4a0ef
Roman Irinarhov <pyshok1991@gmail.com>
2020-08-13 21:59:52 +0300

    Create SearchItem Component

--- client/src/containers/MySearchesContainer/index.js
+++ client/src/containers/MySearchesContainer/index.js
@@ -1,65 +1,15 @@
 import React from 'react';
 import './index.scss';
-import {useDispatch, useSelector} from "react-redux";
-import Button from "@material-ui/core/Button";
-import DeleteIcon from '@material-ui/icons/Delete';
-import Card from "@material-ui/core/Card/Card";
-import Chip from "@material-ui/core/Chip";
+import SearchItem from "../../components/SearchItem";
 
-
-function FilterSearchTags({searchList}) {
-    if (searchList !== undefined) {
-        return (
-            <>
-                {searchList.map((tag, idx) => <Chip className="chip_item" key={idx} label={tag}/>)}
-            </>
-        );
-    }
-    return [];
-
-}
 
 function MySearchesContainer() {
-    const searches = useSelector(state => state.user.searches);
-    const [currentSearchIncTags, setCurrentSearchIncTags] = React.useState(searches[searches.length - 1].include_tags);
-    const [currentSearchExcTags, setCurrentSearchExcTags] = React.useState(searches[searches.length - 1].exclude_tags);
+    debugger
 
-    const onDeleteButtonClicked = () => {
-
-    };
 
     return (
         <div className="searches_container">
-            <Card className="my_searches_card">
-                <h3 className="my_searches_title"><strong>My Last Searches</strong></h3>
-                <div className="my_searches_filter_lists">
-                    <h3 className="my_last_search_title"><strong>Search Name:</strong></h3>
-                    <div className="search_list">
-                        <div className="chip_items">
-                            <h4 className="title_list">
-                                <strong>Include tags:</strong>
-                            </h4>
-                            <FilterSearchTags searchList={currentSearchIncTags}/>
-                        </div>
-                        <div className="chip_items">
-                            <h4 className="title_list">
-                                <strong>Exclude tags:</strong>
-                            </h4>
-                            <FilterSearchTags searchList={currentSearchExcTags}/>
-                        </div>
-                    </div>
-                </div>
-                <div className="card_footer">
-                    <Button className="search_box_submit_btn"
-                            variant="contained"
-                            size="medium"
-                            color="secondary"
-                            startIcon={<DeleteIcon/>}
-                            onClick={onDeleteButtonClicked}>
-                        Delete Search
-                    </Button>
-                </div>
-            </Card>
+           <SearchItem/>
         </div>
     )
 }
