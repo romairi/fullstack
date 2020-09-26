eaf585fe8bab0f7a54d6398498b45ecfd70c8341
Roman Irinarhov <pyshok1991@gmail.com>
2020-09-26 21:16:15 +0300

    Adding settings to bull

--- server/services/updateQueueService.js
+++ server/services/updateQueueService.js
@@ -3,6 +3,17 @@ const {redis} = require('../configs/serverConfig');
 
 const UPDATE_PAPERS_QUEUE_NAME = 'UPDATE_PAPERS';
 
-const getUpdatePapersQueue = () => new Bull(UPDATE_PAPERS_QUEUE_NAME, redis.uri);
+const defaultJobOptions = {
+    removeOnComplete: true,
+    removeOnFail: false,
+};
+
+const limiter = {
+    max: 10000,
+    duration: 1000,
+    bounceBack: false,
+};
+
+const getUpdatePapersQueue = () => new Bull(UPDATE_PAPERS_QUEUE_NAME, redis.uri, {defaultJobOptions, limiter});
 
 module.exports = {getUpdatePapersQueue, UPDATE_PAPERS_QUEUE_NAME};
