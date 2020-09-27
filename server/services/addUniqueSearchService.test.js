fac9e0041931febe35bdb3e613891e04bb4dc0d7
Roman Irinarhov <pyshok1991@gmail.com>
2020-09-27 21:56:34 +0300

    Fix jest test for function addUniqueSearch

--- server/services/addUniqueSearchService.test.js
+++ server/services/addUniqueSearchService.test.js
@@ -1,23 +1,30 @@
 const {uniqueSearch} = require('./addUniqueSearchService');
 
+const searchMock = [
+    {include_tags: ["deep"], exclude_tags: ["dee"],},
+    {include_tags: ["knn", "deep"], exclude_tags: ["deep", "lost", "rgb"],},
+    {include_tags: ["knn"], exclude_tags: ["deep", "lost", "rgb"],},
+];
+
 describe('addUniqueSearchService', () => {
     describe('uniqueSearch', () => {
-        const search = [
-            {include_tags: ["deep"], exclude_tags: ["dee"],},
-            {include_tags: ["knn", "deep"], exclude_tags: ["deep", "lost", "rgb"],},
-            {include_tags: ["knn"], exclude_tags: ["deep", "lost", "rgb"],},
-        ];
 
         it('should return an unique search', () => {
-            expect(uniqueSearch(search, ["knn", "deep"], ["deep", "lost", "rgb"])).toBeFalsy();
-            expect(uniqueSearch(search, ["knn"], ["deep", "lost", "rgb"])).toBeFalsy();
-            expect(uniqueSearch(search, ["knn"], [])).toBeTruthy();
-            expect(uniqueSearch(search, [], ["knn"])).toBeTruthy();
-            expect(uniqueSearch(search, [], [])).toBeTruthy();
-            expect(uniqueSearch(search, ["knn", "deep"], [undefined, null])).toBeTruthy();
-            expect(uniqueSearch(search, [undefined, null], ["knn", "deep"])).toBeTruthy();
-            expect(uniqueSearch(search, [undefined, null, "knn"], ["deep", "lost", "rgb"])).toBeTruthy(); // TODO it must return false
-        });
+            expect(uniqueSearch(searchMock, ["knn", "deep"], ["deep", "lost", "rgb"])).toBeFalsy();
 
+            expect(uniqueSearch(searchMock, ["knn"], [])).toBeTruthy();
+            expect(uniqueSearch(searchMock, ["knn"])).toBeTruthy();
+            expect(uniqueSearch(searchMock, [], ["knn"])).toBeTruthy();
+            expect(uniqueSearch(searchMock, [], [])).toBeTruthy();
+            expect(uniqueSearch(searchMock, ["knn", "deep"], [undefined, null])).toBeTruthy();
+            expect(uniqueSearch(searchMock, [undefined, null, ''], ["knn", "deep"])).toBeTruthy();
+            expect(uniqueSearch(searchMock, [undefined, null, "knn"], ["deep", "lost", "rgb"])).toBeFalsy();
+        });
+        it('should return false - the search already exists', () => {
+            expect(uniqueSearch(searchMock, ["knn"], ["deep", "lost", "rgb"])).toBeFalsy();
+        });
+        it('should return true - the tags exists but not in the same list', () => {
+            expect(uniqueSearch(searchMock, ["deep", "dee"], [])).toBeTruthy();
+        });
     });
 });
