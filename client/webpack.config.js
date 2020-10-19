5b8fd05ad57a944c6200325290b4070a4fbb29f7
Roman Irinarhov <pyshok1991@gmail.com>
2020-10-19 21:18:37 +0300

    Improving performance using dynamic import and minification

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,68 +3,72 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-// const devMode = process.env.NODE_ENV !== 'production';
-const devMode = true;
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, './build'),
-        filename: `static/js/main${devMode ? '' : '.[hash]'}.js`,
-        publicPath: "<%- publicPath %>"
-    },
-    devtool: 'inline-source-map',
-    optimization: {
-        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.scss$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            hmr: process.env.NODE_ENV === 'development',
-                            // reloadAll: true,
-                        },
-                    },
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
+module.exports = env => {
+    const devMode = !env || !env.NODE_ENV  || env.NODE_ENV !== 'production';
+
+    return ({
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, './build'),
+            filename: `static/js/main${devMode ? '' : '.[hash]'}.js`,
+            publicPath: devMode ? 'http://localhost:3000' : '/',
+            chunkFilename: `static/js/chunk-[name]${devMode ? '' : '.[contenthash]'}.js`,
+        },
+        mode: devMode ? 'development' : 'production',
+        devtool: 'cheap-source-map',
+        optimization: {
+            minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
                     }
-                ]
-            },
+                },
+                {
+                    test: /\.scss$/i,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                hmr: devMode,
+                                // reloadAll: true,
+                            },
+                        },
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: "html-loader"
+                        }
+                    ]
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: "./public/index.html",
+                filename: "../../server/views/index.ejs"
+            }),
+            new MiniCssExtractPlugin({
+                filename: `static/css/${devMode ? '[name].css' : '[name].[hash].css'}`,
+                chunkFilename: `static/css/${devMode ? '[id].css' : 'chunk-[name].[id].[hash].css'}`,
+            }),
         ],
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./public/index.html",
-            filename: "../../server/views/index.ejs"
-        }),
-        new MiniCssExtractPlugin({
-            filename: `static/css/${devMode ? '[name].css' : '[name].[hash].css'}`,
-            chunkFilename: `static/css/${devMode ? '[id].css' : '[id].[hash].css'}`,
-        }),
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, 'build'),
-        publicPath: '/',
-        compress: true,
-        port: 3000,
-        hot: true,
-    }
+        devServer: {
+            contentBase: path.join(__dirname, 'build'),
+            publicPath: '/',
+            compress: true,
+            port: 3000,
+            hot: true,
+        }
+    });
 };
