1b1cb026dfe825f7102b6dfc5aaac8ade7ef4b32
Roman Irinarhov <pyshok1991@gmail.com>
2020-11-12 01:42:18 +0200

    init sentry

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import { createStore, compose } from "redux";
 import {Provider} from 'react-redux'
 import './index.scss';
 import store, {history} from './redux/store';
 import App from './containers/Application/';
 import {createSetUserAction} from "./redux/reducers/UserReducer/actions";
+
+
+Sentry.init({
+    dsn: "https://28b1f6296cf24ad584c2016ea94bfb59@o474728.ingest.sentry.io/5513469",
+    integrations: [
+        new Integrations.BrowserTracing(),
+    ],
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+});
+
 
 window.clientData = window.clientData || {};
 
